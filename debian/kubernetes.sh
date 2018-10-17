ffd88f4e016b3aca76bdecf4a4ed9acc7ff8dae0
Marcos Tolosa <marcostolosa@protonmail.com>
2018-10-17 10:29:49 -0300

    adding kubernetes installer

---
+++ debian/kubernetes.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+apt-get update && apt-get install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
+apt-get update
+apt-get install -y kubelet kubeadm kubectl
+apt-mark hold kubelet kubeadm kubectl
